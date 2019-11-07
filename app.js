bcfb5dd4293aed068b1b02fc40dc222055366393
Fran2333 <00161218@uca.edu.sv>
2019-11-07 01:05:34 -0600

    base connection

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import apiRouter from './src/api/Api';
+
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://Fran2333:puky28012008@cluster0-osbzz.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then( ()=> {
+  console.log("Database");
+});
+
+mongoose.Promise =  global.Promise;
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/api', apiRouter);
 
 app.get("/", (req, res) =>{
     res.status(200).send({
@@ -19,3 +33,5 @@ const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
+
+module.exports = app;
