8710cf70ee9150b450b617c5ac31f4a2c3da86a9
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-14 11:35:44 -0600

    Fix: problema con dependencias

--- src/models/location.js
+++ src/models/location.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import User from "./user";
+import Order from "./order";
 
 const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
-  address: String,
-  city: String,
-  department: String
+    address: String,
+    city: String,
+    department: String,
+    employees: [User.schema],
+    orders: [Order.schema]
 });
 
 module.exports = mongoose.model("Location", LocationSchema);
--- src/models/order.js
+++ src/models/order.js
@@ -5,11 +5,11 @@ import Food from "./food";
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-  client: { type: User.schema, required: true },
-  employee: { type: User.schema, required: true },
-  products: { type: [Food.schema], required: true },
-  subtotal: { type: Number, required: true },
-  total: { type: Number, required: true }
+    client: { type: User.schema, required: true },
+    employee: { type: User.schema, required: true },
+    products: { type: [Food.schema], required: true },
+    subtotal: { type: Number, required: true },
+    total: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Order", OrderSchema);
